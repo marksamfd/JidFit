bea07c9f079fb974308a5d15dd415e34c4da2cea
Mark Samuel <62911833+marksamfd@users.noreply.github.com>
2023-12-16 15:48:52 +0200

    - delete plate - view report - fixed #6

--- Pages/nutritionist/DeletePlate.cshtml.cs
+++ Pages/nutritionist/DeletePlate.cshtml.cs
@@ -5,6 +5,14 @@ namespace JidFit.Pages.nutritionist
 {
     public class DeletePlateModel : PageModel
     {
+        [BindProperty(SupportsGet =true)]
+        public string email { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string plateName{ get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string meal{ get; set; }
+		[BindProperty(SupportsGet = true)]
+		public int day{ get; set; }
         public void OnGet()
         {
         }
