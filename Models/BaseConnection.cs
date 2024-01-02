84b45a3bb7470db28405169c8ba1f122288ff755
Mark Samuel <62911833+marksamfd@users.noreply.github.com>
2024-01-02 05:02:45 +0200

    - base connection class - started coachNutritioinst entity class

---
+++ Models/BaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JidFit.Models
+{
+    public class BaseConnection
+    {
+        string ConString;
+        protected SqlConnection con;
+        public BaseConnection()
+        {
+            ConString = "Data Source=DESKTOP-PFKM237\\SQLEXPRESS;Initial Catalog=COMPANYDB;Integrated Security=True";
+            con = new SqlConnection(ConString);
+        }
+    }
+}
